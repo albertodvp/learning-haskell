d116efbef6d263df54dc53458d69f3c7e73841fa
Alberto Fanton <alberto.fanton@protonmail.com>
2020-03-26 08:37:57 +0100

    newtype vs type

---
+++ lhffp/ch11/Newtype.hs
@@ -0,0 +1,30 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE FlexibleInstances #-}
+class TooMany a where
+  tooMany :: a -> Bool
+
+instance TooMany Int where
+  tooMany n = n > 42
+
+
+newtype Goats =
+  Goats Int deriving (Eq, Show, TooMany)
+
+
+-- put this on top
+--{-# LANGUAGE FlexibleInstances #-}
+--instance TooMany (Int, String) where
+--  tooMany = tooMany . fst
+
+
+-- or
+
+newtype IntString =
+  IntString (Int, String) deriving Show
+
+instance TooMany IntString where
+  tooMany (IntString is)= tooMany (fst is)
+                                  
+
+instance (Num a, TooMany a) => TooMany (a, a) where
+  tooMany (x, y) = tooMany (x+y)
