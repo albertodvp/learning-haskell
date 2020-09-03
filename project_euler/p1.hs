a10c060f9be9f0db2061ed923551e8352f15ba1d
Alberto Fanton <alberto.fanton@protonmail.com>
2020-09-03 10:11:08 +0200

    euler p1,p2

---
+++ project_euler/p1.hs
@@ -0,0 +1,14 @@
+module P1 where
+
+import Data.Monoid
+type Predicates = Int -> Bool
+mkFilter :: [Predicates] -> Int -> Bool
+mkFilter p = getAny . foldMap (Any .) p
+
+p1f :: Int -> Bool
+p1f = mkFilter [m 3, m 5]
+  where
+    m x = (==) 0 . flip rem x
+
+p1 :: Int
+p1 = sum $ filter p1f [1..999]
---
+++ project_euler/p2.hs
@@ -0,0 +1,14 @@
+module P2 where
+
+fib1 :: [Int]
+fib1 = 0 : 1 : zipWith (+) fib1 (tail fib1)
+
+fib2 :: [Int]
+fib2 = scanl (+) 0 (1:fib2)
+
+p2 :: Int
+p2 = sum $ w f 
+  where
+    w = takeWhile (< 4000000)
+    f = filter even fib1
+       
