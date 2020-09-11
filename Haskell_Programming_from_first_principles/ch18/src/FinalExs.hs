514c68b38aa596962342df6a0bbafdfe9194cef4
Alberto Fanton <alberto.fanton@protonmail.com>
2020-09-11 11:58:38 +0200

    Uncompleted List exercise

--- Haskell_Programming_from_first_principles/ch18/src/FinalExs.hs
+++ Haskell_Programming_from_first_principles/ch18/src/FinalExs.hs
@@ -1,5 +1,8 @@
 module FinalExs where
 
+import Control.Monad (join)
+
+
 data Nope a = NopeDotJpg deriving (Eq, Show)
 
 instance Functor Nope where
@@ -24,3 +27,53 @@ data BahEither b a = PLeft a | PRight b deriving (Eq, Show)
 instance Functor (BahEither b) where
   fmap _ (PRight b) = PRight b
   fmap f (PLeft a)  = PLeft $ f a
+
+
+instance Applicative (BahEither b) where
+  pure = PLeft
+  PRight f <*> _ = PRight f
+  PLeft f <*> be = fmap f be
+
+instance Monad (BahEither b) where
+  return = pure
+  (PLeft a) >>= fbe = fbe a
+  (PRight b) >>= _  = PRight b
+
+--
+
+newtype Identity a = Identity a deriving (Eq, Show)
+
+instance Functor Identity where
+  fmap f (Identity a) = Identity $ f a
+
+instance Applicative Identity where
+  pure = Identity
+  (Identity f) <*> ida = fmap f ida
+
+instance Monad Identity where
+  return = pure
+  Identity a >>= fid = fid a
+
+
+--
+
+data List a = Nil | Cons a (List a) deriving (Eq, Show)
+
+instance Functor List where
+  fmap _ Nil = Nil
+  fmap f (Cons a xs) = Cons (f a) (fmap f xs)
+
+concat' :: List (List a) -> List a
+concat' = undefined
+
+instance Applicative List where
+  pure = flip Cons Nil
+  Nil       <*> _   = Nil
+  _         <*> Nil = Nil
+  Cons f fs <*> xs  = concat' $ Cons (fmap f xs) (fs <*> xs)
+
+instance Monad List where
+  return = pure
+  Nil >>= _ = Nil
+  (Cons x xs) >>= f = concat' $ Cons (f x) (fmap f xs)
+  
--- Haskell_Programming_from_first_principles/ch18/test/Spec.hs
+++ Haskell_Programming_from_first_principles/ch18/test/Spec.hs
@@ -6,6 +6,8 @@ import Test.QuickCheck
 import Test.QuickCheck.Checkers
 import Test.QuickCheck.Classes
 
+import Control.Applicative (liftA2)
+
 instance (Eq a, Eq b) => EqProp (Sum a b) where
   (=-=) = eq
 
@@ -25,6 +27,21 @@ instance Arbitrary (Nope a) where
 instance (Arbitrary a, Arbitrary b) => Arbitrary (BahEither b a) where
   arbitrary = oneof $ [arbitrary >>= return . PLeft, arbitrary >>= return . PRight]
 
+instance Arbitrary a => Arbitrary (Identity a) where
+  arbitrary = fmap Identity arbitrary
+
+
+instance Arbitrary a => Arbitrary (List a) where
+  arbitrary = frequency xs
+    where
+      xs = [
+        (5, liftA2 Cons arbitrary arbitrary),
+        (1, return Nil)
+        ]
+
+-- EqProp
+
+
 instance Eq a => EqProp (CountMe a) where
   (=-=) = eq
 
@@ -34,6 +51,20 @@ instance EqProp (Nope a) where
 instance (Eq a, Eq b) => EqProp (BahEither b a) where
   (=-=) = eq
 
+instance Eq a => EqProp (Identity a) where
+  (=-=) = eq
+
+take' :: Int -> List a -> List a
+take' 0 xs = Nil
+take' _ Nil = Nil
+take' i (Cons a xs) = Cons a $ take' (i-1) xs
+
+instance Eq a => EqProp (List a) where
+  xs =-= ys = xs' `eq` ys'
+    where
+      xs' = take' 3000 xs
+      ys' = take' 3000 ys
+
 main :: IO ()
 main = do
   let trigger :: CountMe (Int, String, Int)
@@ -42,14 +73,37 @@ main = do
       n = undefined
       be :: BahEither String (Int, String, Int)
       be = undefined
+      i :: Identity (Int, String, Int)
+      i = undefined
+      l :: List (Int, String, Int)
+      l = undefined
+        
   quickBatch $ monad (undefined :: Sum String (String, Int, Char))
+  putStrLn ""
+  putStrLn "CountMe"
   quickBatch $ functor trigger
   quickBatch $ applicative trigger
   quickBatch $ monad trigger
+  putStrLn ""
+  putStrLn "Sum"
   quickBatch $ monad (undefined :: Sum String (String, Int, Char))
+  putStrLn ""
+  putStrLn "Nope"
   quickBatch $ functor n
   quickBatch $ applicative n
   quickBatch $ monad n
+  putStrLn ""
+  putStrLn "BahEither"
   quickBatch $ functor be
-  quickBatch $ applicative n
+  quickBatch $ applicative be
+  quickBatch $ monad be
+  putStrLn ""
+  putStrLn "Identity"
+  quickBatch $ functor i
+  quickBatch $ applicative i
+  quickBatch $ monad i
+  putStrLn ""
+  putStrLn "List"
+  quickBatch $ functor l
+
 
