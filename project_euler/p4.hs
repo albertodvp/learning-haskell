fd14b9728e020eb4cc6e6eee502abfb791109bb2
Alberto Fanton <alberto.fanton@protonmail.com>
2020-09-20 14:18:02 +0200

    p4 euler

--- project_euler/p4.hs
+++ project_euler/p4.hs
@@ -1,7 +1,28 @@
 module P4 where
-import Data.List (sort)
+import Data.List (sortBy)
+
+-- 12321
+-- 1 -> *10 + 2
+mirrored :: Integer -> Integer
+mirrored x = foldl (\a y -> 10*a + y) 0 (f x)
+  where
+    f 0 = []
+    f x = x `rem` 10:f(x `quot` 10)
+
+mirrored' :: Integer -> Integer
+mirrored' x = mir 0 x
+  where
+    mir acc 0 = acc
+    mir acc n = mir (acc * 10 + mod n 10) (div n 10)
+-- isPalindrome :: Integer -> Bool
+-- isPalindrome x = let y = show x in y == reverse y
+isPalindrome  :: Integer -> Bool
+--isPalindrome x = mirrored x == x
+isPalindrome x = mirrored' x == x
+
+
 r = [100..999]
-rr = reverse $ sort [a*b | a <- r, b <- r]
+
 
 p4 :: Integer
-p4 = read $ head $ filter (\x -> x == reverse x) $ map show $ rr
+p4 = foldl1 max  [a*b | a <- r, b <- r, isPalindrome (a*b)]
