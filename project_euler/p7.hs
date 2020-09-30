cb6bc743edd2a5a74f3103d47958caca92846225
Alberto Fanton <alberto.fanton@protonmail.com>
2020-09-30 10:27:43 +0200

    p7 euler

--- project_euler/p7.hs
+++ project_euler/p7.hs
@@ -1,12 +1,22 @@
 module P7 where
 
+divPrimeBy :: Integer -> Integer -> Bool
+divPrimeBy x by = by == x || x `rem` by /= 0
+
 isPrime :: Integer -> Bool
-isPrime 1 = True
+isPrime 1 = False
 isPrime 2 = True
-isPrime x = all ((/= 0) . rem x) [2..x-1]
+isPrime x = all (divPrimeBy x) (takeWhile (<=x `quot` 2)  primes)
 
 primes :: [Integer]
-primes = filter isPrime [1..]
+primes = filter isPrime [2..]
+
+primes' :: [Integer]
+primes' = f [2..]
+  where
+    f (p:xs) = p : f (filter (\x -> mod x p /= 0) xs)
+
 
 p7 :: Integer
-p7 = head $ drop 10000 primes
+p7 = primes !! 10000
+
