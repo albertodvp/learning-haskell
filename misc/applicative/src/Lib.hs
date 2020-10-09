df7728fa91b82d46c5764eb6f161dafe7ef1d42e
Alberto Fanton <alberto.fanton@protonmail.com>
2020-10-09 10:50:22 +0200

    Some ex

--- misc/applicative/src/Lib.hs
+++ misc/applicative/src/Lib.hs
@@ -6,9 +6,20 @@ newtype ZL a = ZL { getZL :: [a] } deriving (Eq, Show)
 -- Compose
 newtype Cmp f g a = Cmp { getCmp :: f (g a) } deriving (Eq, Show)
 
+instance (Functor f, Functor g) => Functor (Cmp f g) where
+  fmap f (Cmp fga)  = Cmp $ fmap (fmap f) fga
+
+instance (Applicative f, Applicative g) => Applicative (Cmp f g) where
+  pure = Cmp . pure . pure
+--  (Cmp fgf) <*> (Cmp fga)  = Cmp $ (fmap . fmap) fgf fga
+  (Cmp fgf) <*> (Cmp fga) = undefined
 -- Bin
 data Bin a = Leaf a | Bin (Bin a) (Bin a) deriving (Eq, Show)
 
+instance Functor Bin where
+  fmap f (Leaf a)    = Leaf $ f a
+  fmap f (Bin ba bb) = Bin (f <$> ba) (f <$> bb) 
+  
 -- BinL
 data BinL a = BinL a (BinL a) (BinL a) | LeafL deriving (Eq, Show)
 
--- misc/applicative/test/Main.hs
+++ misc/applicative/test/Main.hs
@@ -1,3 +1,17 @@
 import Lib
+
+import Test.QuickCheck
+import Test.QuickCheck.Checkers
+import Test.QuickCheck.Classes
+
+instance Arbitrary (Cmp f g a) where
+  arbitrary = undefined
+
+instance EqProp (Cmp f g a) where
+  (=-=) (Cmp fga) (Cmp fgb) = undefined
+
+
 main :: IO()
-main = someFunc
+main = do
+  -- quickBatch $ functor (undefined :: Cmp Maybe (Either String) (Int, String, Char) )
+  -- quickBatch $ applicative (undefined :: Cmp Maybe (Either String) (Int, String, Char) )
