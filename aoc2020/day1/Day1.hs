e0ecccc5d72c36ded3306ae991355085d1fabd4d
Alberto Fanton <alberto.fanton@protonmail.com>
2020-12-05 01:05:23 +0100

    Ex1 aoc cleanup

--- aoc2020/day1/Day1.hs
+++ aoc2020/day1/Day1.hs
@@ -1,26 +1,90 @@
-type Resolver = [Int] -> Int
+import           Control.Applicative (liftA2)
+import           Control.Monad       (guard, sequence)
+import           Criterion.Main
+import           Data.Foldable       (foldMap)
+import           Data.Functor        ((<&>))
+import qualified Data.HashTable.IO   as H
+import qualified Data.IntSet         as IntSet
+import qualified Data.Map            as Map
+import           Data.Maybe          (catMaybes, isJust)
+import           Data.Monoid
+type Resolver = [Int] -> IO Int
+type Target = Int
+type HashTable = H.CuckooHashTable Int (Int, Int)
 
+
+--
+-- PROBLEM 1
+--
 -- Correct answer: 440979
+-- V1
 -- Time complexity: O(n) = n^2
-findRes1 :: Resolver
-findRes1 xs = head [x*y
-                   | x <- xs,
-                     y <- xs,
-                     x + y == 2020, x /= y]
+findRes1 :: Target -> Resolver
+findRes1 t xs = pure $ head $ do
+  x <- xs
+  y <- xs
+  guard $ x + y == t
+  guard $ x /= y
+  pure $ x*y
 
+-- V2
+-- Time complexity: O(n*min(m,W))  with lenght xs = n, lenght diffs = m, W = the number of bits of an Int
+findRes1Fast :: Target -> Resolver
+findRes1Fast t xs = pure $ x * (t - x)
+  where
+    x = head $ filter (`IntSet.member` diffs) xs -- O(1) + O(n*min(m,W))
+    diffs = IntSet.fromList $ (t -) <$> xs -- O(n*min(n,W)) + O(n)
+
+--
+-- PROBLEM 2
+--
 -- Correct answer: 82498112
 -- Time complexity: O(n) = n^3
-findRes2 :: Resolver
-findRes2 xs = head [x*y*z
-                   | x <- xs,
-                     y <- xs,
-                     z <- xs,
-                     x + y + z == 2020, x /= y, y /= z, z /= x]
+findRes2 :: Target -> Resolver
+findRes2 t xs = pure $ head $ do
+  x <- xs
+  y <- xs
+  z <- xs
+  guard $ x + y + z == t
+  guard $ x /= y
+  guard $ y /= z
+  guard $ z /= x
+  pure $ x*y*z
 
-day1 :: Resolver -> String -> String
-day1 p s = show $ p nums
+-- Time complexity: O(n^2) (I expected)
+findRes2Fast :: Target -> Resolver
+findRes2Fast t xs = fmap (head . catMaybes) $ sequence $ do
+      x <- xs -- O(n^2)
+      pure $ do
+        ht <- iht
+        myz <- H.lookup ht x -- O(1) https://hackage.haskell.org/package/hashtables-1.2.4.1/docs/Data-HashTable-Class.html#v:lookup
+        pure $ do
+          yz <- myz
+          pure $ x * (t - fst yz) * snd yz
+
   where
-    nums = map read (lines s)
+    iht = H.fromList diffs :: IO HashTable  -- O(n)
+    diffs = liftA2 subSave partialDiffs xs -- O(n^2)
+    partialDiffs = (t -) <$> xs -- O(n)
+    subSave x y = (x - y, (x,y))
 
-main :: IO()
-main = interact $ day1 findRes2
+
+resolvers :: Target -> Map.Map String Resolver
+resolvers t = Map.fromList
+  [ ("day1.1 fast", findRes1Fast t)
+  , ("day1.2 fast (actually slower)", findRes2Fast t)
+  , ("day1.1", findRes1 t)
+  , ("day1.2", findRes2 t)
+  ]
+
+inputFile :: FilePath
+inputFile = "./input"
+
+inputs :: IO [Int]
+inputs = fmap read <$> (lines <$> readFile inputFile)
+
+results :: IO (Map.Map String Int)
+results = inputs >>= sequence . (<$> resolvers 2020) . flip ($)
+
+main :: IO ()
+main = defaultMain $ uncurry bench <$> Map.toList (whnfIO . (inputs >>=) <$> resolvers 2020)
