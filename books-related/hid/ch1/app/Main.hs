8b181d88b370a4d111d66755ec4680a434121a0f
Alberto Fanton <alberto.fanton@protonmail.com>
2022-12-02 02:00:26 +0100

    Finish hid-ch1 examples

--- books-related/hid/ch1/app/Main.hs
+++ books-related/hid/ch1/app/Main.hs
@@ -1,18 +1,24 @@
 module Main (main) where
 
-import Data.Char
-import Data.List (group, sort)
 import qualified Data.Text as T
 import qualified Data.Text.IO as TIO
-import System.Environment
+import System.Environment ( getArgs )
+import Control.Monad (when)
+import Lib
+
+processTextFile :: FilePath -> Bool -> Int ->  IO ()
+processTextFile fname withAllWords n = do
+  text <- TIO.readFile fname
+  let vocab = extractVocab text
+  when withAllWords $ TIO.putStrLn $ allWordsReport vocab
+  TIO.putStrLn $ wordsCountReport vocab
+  TIO.putStrLn $ frequentWordsReport vocab n  
+
 
 main :: IO ()
 main = do
-  [fname] <- getArgs
-  text <- TIO.readFile fname
-  let ws = map head $ group $ sort $ map T.toCaseFold $ filter (not . T.null)
-           $ map (T.dropAround $ not . isLetter) $ T.words text
-  TIO.putStrLn $ T.unwords ws
-  print $ length ws  
-
-
+  args <- getArgs
+  case args of
+    ["-a", fname, num] -> processTextFile fname True (read num)
+    [fname, num] -> processTextFile fname False (read num)
+    _ -> putStrLn "Usage [-a] filename frequ_words_num"
--- books-related/hid/ch1/src/Lib.hs
+++ books-related/hid/ch1/src/Lib.hs
@@ -1,6 +1,53 @@
-module Lib
-    ( someFunc
-    ) where
+{-# LANGUAGE OverloadedStrings #-}
+module Lib where
+import Data.Text (Text)
+import Fmt
+import qualified Data.Text as T
+import qualified Data.Text.IO as TIO
+import Data.Char (isLetter)
+import Data.List (sortBy, sort, group)
+import Data.Ord
+type Entry = (T.Text, Int)
+type Vocabulary = [Entry]
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+extractVocab :: Text -> Vocabulary
+extractVocab t = map buildEntry $ group $ sort ws''
+  where
+    ws = map cleanWord (T.words t)
+    ws' = filter (not . T.null) ws
+    ws'' = map T.toCaseFold ws'
+    cleanWord = T.dropAround (not . isLetter)
+    buildEntry xs@(x:_) = (x, length xs)
+
+    
+allWords :: Vocabulary -> [Text]
+allWords = map fst
+
+wordsCount :: Vocabulary -> (Int, Int)
+wordsCount v = (total, unique)
+  where
+    total = sum $ map snd v
+    unique = length $ map fst v    
+
+wordsByFrequency :: Vocabulary -> Vocabulary
+wordsByFrequency = sortBy (comparing $ Down . snd)
+
+-- Reports
+allWordsReport :: Vocabulary -> Text
+allWordsReport v = fmt $ nameF "All words" $ unlinesF (allWords v)
+
+wordsCountReport :: Vocabulary -> Text
+wordsCountReport v = fmt $
+  "Total number of words" +|total|+
+  "\nNumber of unique words: " +|unique|+ "\n"
+  where
+    (total, unique) = wordsCount v
+  
+frequentWordsReport :: Vocabulary -> Int -> Text
+frequentWordsReport v n =
+  fmt $ nameF "Frequent words"
+      $ blockListF' "" fmtEntry reportData
+  where
+    reportData = take n $ wordsByFrequency v
+    fmtEntry (t, n) = ""+|t|+": "+|n|+ ""
+
