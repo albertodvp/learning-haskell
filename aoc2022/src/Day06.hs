9ee22c558d660126d916ee07be212b493e5ea719
Alberto Fanton <alberto.fanton@protonmail.com>
2022-12-07 17:40:31 +0100

    Add day06

---
+++ aoc2022/src/Day06.hs
@@ -0,0 +1,26 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+
+module Day06 (day06) where
+
+import Protolude
+
+import qualified Data.List as L
+import qualified Data.Set as S
+import Data.Text (unpack)
+import Data.Maybe (fromJust)
+firstNDiff :: Int -> [Char] -> Maybe Int
+firstNDiff dim = go 0
+  where
+    go _ [] = Nothing
+    go index xs
+        | S.size (S.fromList $ take dim xs) == dim = Just $ index + dim
+        | otherwise = go (index + 1) $ L.tail xs
+
+p1 :: [Char] -> Int
+p1 = fromJust . firstNDiff 4
+p2 :: [Char] -> Int
+p2 = fromJust . firstNDiff 14
+
+day06 :: IO ()
+day06 = readFile "inputs/day06.txt" >>= print . liftA2 (,) (p1) (p2) . unpack
--- aoc2022/src/Main.hs
+++ aoc2022/src/Main.hs
@@ -5,11 +5,17 @@ import Day02 (day02)
 import Day03 (day03)
 import Day04 (day04)
 import Day05 (day05)
+import Day06 (day06)
 
-main :: IO ()
-main = do
-    day01
-    day02
-    day03
-    day04
-    day05
+days :: [IO ()]
+days =
+    [ day01
+    , day02
+    , day03
+    , day04
+    , day05
+    , day06
+    ]
+
+main :: IO [()]
+main = sequence days
