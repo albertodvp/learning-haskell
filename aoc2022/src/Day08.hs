58ee6cab5dfbb1b5fd8688e515f61b0253edc652
Alberto Fanton <alberto.fanton@protonmail.com>
2022-12-08 18:51:33 +0100

    Refactor

--- aoc2022/src/Day08.hs
+++ aoc2022/src/Day08.hs
@@ -10,6 +10,7 @@ import Text.Megaparsec.Char (eol, numberChar)
 type Parser = Parsec Void Text
 type Trees = M.Matrix Char
 type Index = (Int, Int)
+type Solver = Trees -> [Index] -> Int
 
 parseGame :: Parser Trees
 parseGame = M.fromLists <$> many (many numberChar <* eol)
@@ -28,9 +29,6 @@ compareVisible trees base candidate = let getValue = (M.!) trees in getValue can
 isVisible :: Ord a => M.Matrix a -> Index -> Bool
 isVisible trees i = or $ all (compareVisible trees i) <$> getTargetIndexes i trees
 
-p1 :: Trees -> Int
-p1 trees = length $ filter (isVisible trees) [(i, j) | i <- [1 .. M.nrows trees], j <- [1 .. M.ncols trees]]
-
 takeWhile1 :: (a -> Bool) -> [a] -> [a]
 takeWhile1 _ [] = []
 takeWhile1 f (x : xs)
@@ -40,16 +38,17 @@ takeWhile1 f (x : xs)
 scenicScore :: Ord a => M.Matrix a -> Index -> Int
 scenicScore trees i = product $ length . takeWhile1 (compareVisible trees i) <$> getTargetIndexes i trees
 
-p2 :: Trees -> Int
-p2 trees =
-    maximum $
-        [ scenicScore trees (i, j)
-        | i <- [1 .. M.nrows trees]
-        , j <- [1 .. M.ncols trees]
-        ]
+p1 :: Solver
+p1 trees = length . filter (isVisible trees)
+p2 :: Solver
+p2 trees = maximum . map (scenicScore trees)
 
+play :: Solver -> Trees -> Int
+play solver trees = solver trees [(i, j) | i <- [1 .. M.nrows trees], j <- [1 .. M.ncols trees]]
+
+-- type Solver = Trees -> [Index] -> Int
 day08 :: IO ()
 day08 =
     readFile "inputs/day08.txt" >>= \t -> case parse parseGame "" t of
         Left err -> print $ errorBundlePretty err -- NOTE: assume the output is syntatically correct
-        Right trees -> print (p1 trees, p2 trees)
+        Right trees -> print (play p1 trees, play p2 trees)
