450f346288099fadfd071d65d120d348be1624a6
Alberto Fanton <alberto.fanton@protonmail.com>
2022-12-27 21:43:31 +0100

    Ch3 finish main

--- books-related/hid/ch3/src/Main.hs
+++ books-related/hid/ch3/src/Main.hs
@@ -1,16 +1,41 @@
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
 import Data.Foldable (toList)
 import Data.Csv (decodeByName)
 import qualified Data.ByteString.Lazy as BL
 import QuoteData
+import Data.Text (unpack)
+import Params
+import Control.Monad (unless, when)
+import HtmlReport
+import StatReport
+import Charts
 
 main :: IO ()
-main = putStrLn "Stock quotes processing project"
+main = cmdLineParser >>= work
 
-readQuotes :: FilePath -> IO [QuoteData]
-readQuotes fp = do
-  csvData <- BL.readFile fp
+work :: Params -> IO ()
+work params = do
+  csvData <- BL.readFile (fname params)
   case decodeByName csvData of
-    Left _ -> pure []
-    Right (_, qd) -> pure $ toList qd
+    Left err -> putStrLn err
+    Right (_, quotes) -> generateReports params quotes
+
+
+generateReports :: (Functor t, Foldable t)  => Params -> t QuoteData -> IO ()
+generateReports Params {..} quotes = do
+  unless silent $ putStr textRpt
+  when chart $ plotChart title quotes chartFname
+  saveHtml htmlFile htmlRpt
+  where
+    statInfo' = statInfo quotes
+    textRpt = textReport statInfo'
+    htmlRpt = htmlReport title quotes statInfo' [chartFname | chart]
+    withCompany prefix = maybe mempty (prefix <>) company
+    chartFname = unpack $ "chart" <> withCompany "_" <> ".svg"
+    title = unpack $ "Historical Quotes" <> withCompany " for "
+    saveHtml Nothing _ = pure ()
+    saveHtml (Just f) html = BL.writeFile f html
+    
--- books-related/hid/ch3/src/Params.hs
+++ books-related/hid/ch3/src/Params.hs
@@ -1,4 +1,25 @@
--- | 
-
 module Params where
+import Options.Applicative
+import Data.Text (Text, strip)
 
+data Params = Params {
+    fname :: FilePath
+  , company :: Maybe Text
+  , chart :: Bool
+  , htmlFile :: Maybe FilePath
+  , silent :: Bool
+  }
+  
+mkParams :: Parser Params
+mkParams = Params
+           <$> strArgument (metavar "FILE" <> help "CSV file name")
+           <*> optional (strip <$> strOption (long "name" <> short 'n' <> help "companan name"))
+           <*> switch (long "chart" <> short 'c' <> help "generate chart")
+           <*> optional (strOption $ long "html" <> metavar "FILE" <> help "generate HTML report")
+           <*> switch (long "silent" <> short 's' <> help "don't print statistics")
+
+cmdLineParser :: IO Params
+cmdLineParser = execParser opts
+  where
+    opts = info (mkParams <**> helper)
+                (fullDesc <> progDesc "Stock quotes data processing")
