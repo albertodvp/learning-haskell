2854cf3c0c49a420786d4b6142056949260135b8
Alberto Fanton <alberto.fanton@protonmail.com>
2023-02-15 18:14:16 +0100

    Finish ch4

--- books-related/hid/ch4/statistics/src/Lib.hs
+++ books-related/hid/ch4/statistics/src/Lib.hs
@@ -1,6 +1,24 @@
-module Lib
-    ( someFunc
-    ) where
+module Lib(simple) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import qualified Data.Vector as V
+import           Parsing     (Gender (..), Person (..), decodePersonVector)
+
+data Result = Result
+  { nFemale      :: Int
+  , oldestPerson :: Maybe Person
+  }
+
+
+compute :: Result -> Person -> Result
+ (Result nFemale mOldest) newPerson = Result newCount $ Just newOld
+  where
+    newCount = case gender newPerson of
+      M -> nFemale
+      F -> nFemale + 1
+    newOld = case mOldest of
+      Nothing        -> newPerson
+      Just accPerson -> min accPerson newPerson
+
+simple :: V.Vector Person -> Result
+simple = foldl compute (Result 0 Nothing)
+
--- books-related/hid/ch4/statistics/src/Parsing.hs
+++ books-related/hid/ch4/statistics/src/Parsing.hs
@@ -2,7 +2,7 @@
 {-# LANGUAGE DeriveGeneric     #-}
 {-# LANGUAGE OverloadedStrings #-}
 
-module Parsing(decodePersonVector) where
+module Parsing(decodePersonVector, Person(..), Gender(..)) where
 
 import           Data.ByteString.Char8 (unpack)
 import qualified Data.ByteString.Lazy  as BL
@@ -10,13 +10,14 @@ import           Data.Csv
 import           Data.Time             (Day, defaultTimeLocale, parseTimeM)
 import qualified Data.Vector           as V
 import           GHC.Generics          (Generic)
-data Gender = M | F deriving Show
+import           Lib                   (simple)
+data Gender = M | F deriving (Show, Eq)
 
 data Person = Person
   { name      :: String
   , gender    :: Gender
   , birthDate :: Day
-  } deriving (Generic, FromRecord, Show)
+  } deriving (Generic, FromRecord, Show, Eq)
 
 instance FromField Gender where
   parseField "Male"   = pure M
@@ -29,3 +30,6 @@ decodePersonVector :: BL.ByteString -> V.Vector Person
 decodePersonVector csvData = case decode NoHeader csvData of
   Left err -> error err
   Right v  -> v
+
+instance Ord Person where
+  p1 <= p2 = birthDate p1 <= birthDate p2
