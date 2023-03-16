8b87937f5710bdd001032c469b00aca3576f2645
Alberto Fanton <alberto.fanton@protonmail.com>
2023-03-16 23:50:27 +0100

    Tmp prop testing

--- books-related/hid/ip/lookup/src/IPTypes.hs
+++ books-related/hid/ip/lookup/src/IPTypes.hs
@@ -1,13 +1,14 @@
 {-# LANGUAGE DeriveAnyClass #-}
 module IPTypes where
 import           Control.Exception (Exception)
+import           Data.List
 import           Data.Word
 
-newtype IP = IP { unIP :: Word32 } deriving (Eq, Ord, Show)
+newtype IP = IP { unIP :: Word32 } deriving (Eq, Ord)
 
-data IPRange = IPRange IP IP deriving (Eq, Show)
+data IPRange = IPRange IP IP deriving Eq
 
-newtype IPRangeDB = IPRangeDB [IPRange] deriving (Eq, Show)
+newtype IPRangeDB = IPRangeDB [IPRange] deriving Eq
 
 type LineNumber = Int
 
@@ -22,3 +23,17 @@ instance Show InvalidArgsExceptions where
   show (InvalidIP s) = "Invalid IP address to check: " ++ s
 
 
+
+instance Show IP where
+  show (IP ip) = intercalate "." (map show [b4, b3, b2, b1])
+    where
+      (ip1, b1) = ip `divMod` 256
+      (ip2, b2) = ip1 `divMod` 256
+      (b4, b3) = ip2 `divMod` 256
+
+
+instance Show IPRange where
+  show (IPRange ip1 ip2) = show ip1 ++ "," ++ show ip2
+
+instance Show IPRangeDB where
+  show (IPRangeDB iprs) = unlines $ map show iprs
